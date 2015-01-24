80f6f6e826448966a87815e52ae8b85c312c35b1
Daniel Tschan <tschan@puzzle.ch>
2015-01-24 14:03:03 +0100

    Added TimeServlet.

---
+++ src/main/java/com/puzzleitc/servlet/TimeServlet.java
@@ -0,0 +1,30 @@
+package com.puzzleitc.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/")
+public class TimeServlet extends HttpServlet {
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html><head><title>Time</title></head><body>");
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss %z");
+        Calendar cal = Calendar.getInstance();
+        writer.println(dateFormat.format(cal.getTime()));
+        writer.println("</body></html>");
+        writer.close();
+     }
+ }

fa65f3721f2335c440bbba11f64a1a076b38d88b
Daniel Tschan <tschan@puzzle.ch>
2016-05-18 23:00:32 +0200

    Add JSON logging.

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-The OpenShift `jbosseap` cartridge documentation can be found at:
-
-http://openshift.github.io/documentation/oo_cartridge_guide.html#jbosseap
+OpenShift 3 Java EE Example demonstrating JSON logging to stdout.
--- src/main/java/com/puzzleitc/servlet/TimeServlet.java
+++ src/main/java/com/puzzleitc/servlet/TimeServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,13 +13,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.message.MapMessage;
+
 @SuppressWarnings("serial")
 @WebServlet("/")
 public class TimeServlet extends HttpServlet {
-    
+
+    public static final Logger logger = LogManager.getFormatterLogger(TimeServlet.class);
+
+    public static void main(String[] args) {
+      logger.info(map("a", "1", "b", "2").getFormattedMessage(new String[]{"JSON"}));
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       resp.setContentType("text/html");
+        logger.info(map("url", req.getRequestURL().toString()).getFormattedMessage(new String[]{"JSON"}));
+        resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println("<html><head><title>Time</title></head><body>");
         DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss 'UTC'XXX");
@@ -26,5 +38,32 @@ public class TimeServlet extends HttpServlet {
         writer.println(dateFormat.format(cal.getTime()));
         writer.println("</body></html>");
         writer.close();
-     }
+    }
+
+
+    private static MapMessage map(String... data){
+      HashMap<String, String> result = new HashMap<String, String>();
+
+      if(data.length % 2 != 0) 
+          throw new IllegalArgumentException("Odd number of arguments");      
+
+      String key = null;
+      Integer step = -1;
+
+      for(String value : data){
+          step++;
+          switch(step % 2){
+          case 0: 
+              if(value == null)
+                  throw new IllegalArgumentException("Null key value"); 
+              key = value;
+              continue;
+          case 1:             
+              result.put(key, value);
+              break;
+          }
+      }
+
+      return new MapMessage(result);
+    }
  }

dbef45009ea780d2a0f3058c5cfbcf61ea620186
Daniel Tschan <tschan@puzzle.ch>
2016-05-19 10:54:24 +0200

    Add context log info.

--- src/main/java/com/puzzleitc/servlet/TimeServlet.java
+++ src/main/java/com/puzzleitc/servlet/TimeServlet.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.message.MapMessage;
+import org.apache.logging.log4j.ThreadContext;
 
 @SuppressWarnings("serial")
 @WebServlet("/")
@@ -24,13 +25,18 @@ public class TimeServlet extends HttpServlet {
     public static final Logger logger = LogManager.getFormatterLogger(TimeServlet.class);
 
     public static void main(String[] args) {
-      logger.info(map("a", "1", "b", "2").getFormattedMessage(new String[]{"JSON"}));
+      ThreadContext.put("freeMem", String.valueOf(Runtime.getRuntime().freeMemory()));
+
+      logger.info(map("a", "1", "b", "2"));
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        logger.info(map("url", req.getRequestURL().toString()).getFormattedMessage(new String[]{"JSON"}));
-        System.out.println("{\"timeMillis\":1463644362907,\"thread\":\"default task-4\",\"level\":\"INFO\",\"loggerName\":\"com.puzzleitc.servlet.TimeServlet\",\"message\":{\"url\":\"http://ose3-java-logging-dtschan.ose3-lab.puzzle.ch/\"},\"endOfBatch\":false,\"loggerFqcn\":\"org.apache.logging.log4j.spi.AbstractLogger\",\"contextMap\":[],\"source\":{\"class\":\"com.puzzleitc.servlet.TimeServlet\",\"method\":\"doGet\",\"file\":\"TimeServlet.java\",\"line\":32}}");
+      ThreadContext.put("url", req.getRequestURL().toString());
+      ThreadContext.put("freeMem", String.valueOf(Runtime.getRuntime().freeMemory()));
+
+      logger.info(map("url", req.getRequestURL().toString()));
+
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println("<html><head><title>Time</title></head><body>");
@@ -39,6 +45,9 @@ public class TimeServlet extends HttpServlet {
         writer.println(dateFormat.format(cal.getTime()));
         writer.println("</body></html>");
         writer.close();
+
+
+      ThreadContext.clearAll();
     }
 
 
